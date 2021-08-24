96d255699d2a21d33fd34b26713a7e0ce1431f0d
Ilia Choly <ilia.choly@gmail.com>
2021-08-24 11:00:40 -0400

    add tests

---
+++ tests/specs/primitives/output.ts
@@ -0,0 +1,20 @@
+export interface GetSomeThingPath {
+    b?: number;
+}
+
+export interface GetSomeThingQuery {
+    a: string;
+}
+
+export interface GetSomeThingBody {
+    c?: boolean;
+}
+
+export interface GetSomeThingResponse {}
+
+export interface GetSomeThingRequest
+    extends GetSomeThingQuery,
+    GetSomeThingPath {
+    body: GetSomeThingBody;
+}
+    
--- tests/test.js
+++ tests/test.js
@@ -8,10 +8,11 @@ chai.use(require("chai-diff"));
 const expect = chai.expect;
 
 async function runTransformTest(spec) {
-  const dir = path.join("specs", spec);
+  const dir = path.join(__dirname, "specs", spec);
   const expected = await fs.promises.readFile(path.join(dir, "output.ts"), "utf-8");
   const code = await transform(await load(path.join(dir, "input.json")));
   expect(code).not.differentFrom(expected);
 }
 
 it("should transform an empty spec", async () => await runTransformTest("empty"));
+it("should transform primitive parameters", async () => await runTransformTest("primitives"));
