fafcc2879561730deb2f3e601f514bd5f71be956
Ilia Choly <ilia.choly@gmail.com>
2021-08-24 11:25:27 -0400

    re-add main into src

--- bin/index.js
+++ bin/index.js
@@ -1,13 +1,3 @@
 #!/usr/bin/env node
 
-const { load, transform } = require("../lib");
-
-async function main() {
-    for (const filename of process.argv.slice(2)) {
-        const doc = await load(filename);
-        const code = await transform(doc);
-        console.log(code);
-    }
-}
-
-main();
+require("../lib/main");
---
+++ src/main.ts
@@ -0,0 +1,12 @@
+
+import { load, transform } from "./index";
+
+async function main() {
+    for (const filename of process.argv.slice(2)) {
+        const doc = await load(filename);
+        const code = await transform(doc);
+        console.log(code);
+    }
+}
+
+main();
