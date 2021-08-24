ad852b4123ce5c910a78ae07bf538f72a6f65e4f
Ilia Choly <ilia.choly@gmail.com>
2021-08-24 12:15:32 -0400

    update output

--- tests/specs/allof/output.ts
+++ tests/specs/allof/output.ts
@@ -1,3 +1,16 @@
 export interface ItemsWithIssue extends OrderedItem {
-    reason?: string;
+  reason?: string;
 }
+
+export interface PutSomePathPath {}
+
+export interface PutSomePathQuery {}
+
+export interface PutSomePathBody {}
+
+export interface PutSomePathResponse {}
+
+export interface PutSomePathRequest extends PutSomePathQuery, PutSomePathPath {
+  body: PutSomePathBody;
+}
+
