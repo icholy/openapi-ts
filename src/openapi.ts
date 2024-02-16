b00e22a7707ab313d6fa1a2ee56848775d3d0fff
Ilia Choly <ilia.choly@gmail.com>
2024-02-16 15:51:25 -0500

    feat: add support for x-deprecated

--- src/openapi.ts
+++ src/openapi.ts
@@ -16,3 +16,15 @@ export function isMethod(method: string): method is Method {
   return methods.has(method);
 }
 
+export type DeprecatableObject = OpenAPIV3.SchemaObject | OpenAPIV3.ArraySchemaObject | OpenAPIV3.NonArraySchemaObject | OpenAPIV3.ParameterObject;
+
+export function isDeprecatedObject(obj: DeprecatableObject): boolean {
+  if (typeof obj.deprecated === 'boolean') {
+    return obj.deprecated;
+  }
+  if ('x-deprecated' in obj && typeof obj['x-deprecated'] === 'boolean') {
+    return obj['x-deprecated'];
+  }
+  return false;
+}
+
